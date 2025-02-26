674b2bab1896e45553d7188af9e1447d72597663
krissHernandez <krishernandez122901@gmail.com>
2025-02-25 22:06:03 -0700

    Integracion de la extension de color

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose'
+import Colors  from 'colors'
 
 export const connectDB = async ()=>{
     try {
         const {connection} = await mongoose.connect(process.env.MONGO_URI)
         const url=`${connection.host}:${connection.port}`
-        console.log(`MongoDB Conectado en ${url}`)
+        console.log(Colors.cyan.bold(`MongoDB Conectado en ${url}`))
     } catch (error) {
         console.log(error)
         process.exit(1)
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
+import colors from 'colors'
 import server from './server'
 const port = process.env.PORT || 4000//definimos puerto usado en computadora 
 
 server.listen(port,()=>{//creacion del servidor web vistar localhost:4000 nota se puede usar cualquier puerto
-    console.log('Servidor funcionando en el puerto:',port)
+    console.log(colors.magenta.bold('Servidor funcionando en el puerto:'),port)
 })
 
 
 
+type product = {
+    id:number
+    price:number
+    name: string
+}
 
+let product : product= {
+    id : 1,
+    price : 30,
+    name: "tablet"
+}
+
+const numero=[1,2,3,4]
